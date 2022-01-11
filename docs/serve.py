b7099dac6273fa0d72fb0d303ece981fd68cc965
Ralph Seichter <gitlab@seichter.de>
2022-01-11 15:55:04 +0100

    Small code refactoring

--- docs/serve.py
+++ docs/serve.py
@@ -2,15 +2,17 @@ from livereload import Server
 from livereload import shell
 
 
-def watch(what: str) -> None:
-    server.watch(f'_source/{what}', shell('make html'), delay=2)
+def watch(s: Server, what: str) -> None:
+    s.watch(f'_source/{what}', shell('make html'), delay=2)
+
+
+def serve() -> None:
+    s = Server()
+    watch(s, '*.rst')
+    watch(s, '_static/*')
+    watch(s, '_templates/*')
+    s.serve(root='_build/html')
 
 
 if __name__ == '__main__':
-    server = Server()
-    watch('*.md')
-    watch('*.py')
-    watch('*.rst')
-    watch('_static/*')
-    watch('_templates/*')
-    server.serve(root='_build/html')
+    serve()
