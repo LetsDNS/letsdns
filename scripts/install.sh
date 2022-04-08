1fea1f61f77f2be59ed34bbf876d0199b98cd770
Ralph Seichter <gitlab@seichter.de>
2022-04-08 16:59:14 +0200

    Add install script

---
+++ scripts/install.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# vim:ft=sh:ts=4:sw=4:noet
+#
+# Install LetsDNS from scratch.
+
+set -euo pipefail
+
+function die {
+	echo >&2 "$@"
+	exit 1
+}
+
+function usage {
+	die "Usage: $(basename ${0}) {destination-directory}"
+}
+
+function _install {
+	local d="${1}"
+	[ -d "${d}" ] && die "Directory ${d} already exists, exiting."
+	mkdir -p "${d}"
+	pushd >/dev/null "${d}"
+	python3 -m venv .venv
+	source .venv/bin/activate
+	pip install --pre letsdns
+	letsdns -h
+	popd >/dev/null
+}
+
+[ $# -ge 1 ] || usage
+_install "${1}"
