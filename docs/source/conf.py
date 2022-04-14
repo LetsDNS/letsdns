914f368dafada1d2be0eee76fbc64fea799112bf
Ralph Seichter <gitlab@seichter.de>
2022-04-14 15:27:00 +0200

    Sphinx configuration tweaks

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,29 +7,25 @@ from letsdns import VERSION
 
 sys.path.insert(0, os.path.abspath('..'))
 
+author = 'Ralph Seichter'
+language = 'en'
 project = 'LetsDNS'
 project_copyright = '2022 Ralph Seichter'
-author = 'Ralph Seichter'
 
 # The short X.Y version
 version = 'latest'
 # The full version, including alpha/beta/rc tags
 release = VERSION
 
-extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
-]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files. This pattern also
-# affects html_static_path and html_extra_path.
 exclude_patterns = [
     ".DS_Store",
     "_build",
     "build",
 ]
+extensions = ['sphinx.ext.autodoc']
 
 html_favicon = 'favicon.ico'
 html_static_path = ['_static']
+html_last_updated_fmt = '%Y-%m-%d'
 html_theme = 'sphinx_rtd_theme'
+html_title = 'LetsDNS'
